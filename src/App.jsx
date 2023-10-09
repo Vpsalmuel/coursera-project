868568061fdba013858825f786e1c9f66ba3a530
Samuel Daniel <40503295+Vpsalmuel@users.noreply.github.com>
2023-10-09 14:17:14 +0100

    cleanedup the files

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
 import React from "react";
 
-// import Pizza from "./component/Pizza";
-import Header from "./component/Header";
-import Footer from "./component/Footer";
-import Menu from "./component/Menu";
-
 const App = () => {
-  return (
-    <div className="container">
-      <Header />
-      <Menu />
-
-      <Footer />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
