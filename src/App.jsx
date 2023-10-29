e90bd8827d1d2582bbba57aad6bd2537bedc85c4
Samuel Daniel <40503295+Vpsalmuel@users.noreply.github.com>
2023-10-29 13:29:39 +0100

    added new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Header from "./Component/Header";
+import Main from "./Component/Main";
+import Footer from "./Component/Footer";
+import Nav from "./Component/Nav";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
